90ba50fecbec9469edfd4abaf4e0c054aeaa4012
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-19 11:56:33 -0300

    update: background not finalized

--- background.js
+++ background.js
@@ -1,9 +1,27 @@
+importScripts("utils.js");
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.action === "getCompatibility") {
-    fetch(`https://www.last.fm/user/${msg.username}`, {  credentials: "include" })
+  if (msg.action === "checkLogin") {
+    fetch(`https://www.last.fm/`, { credentials: "include" })
       .then(response => response.text())
       .then(html => {
-        sendResponse({ html });
+        if (!html.includes('href="/login"')) {
+          sendResponse({ loggedIn: true })
+        } else {
+          sendResponse({ loggedIn: false })
+        }
+      });
+    return true;
+  }
+
+  if (msg.action === "getCompatibilityScrapping") {
+    fetch(`https://www.last.fm/user/${msg.username}`, { credentials: "include" })
+      .then(response => response.text())
+      .then(html => {
+        const htmlParseFromString = htmlParser(html);
+        console.log
+        const userCompatData = {};
+
       })
       .catch(err => {
         sendResponse({ error: err.toString() });
@@ -11,18 +29,9 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     return true;
   }
 
-  if(msg.action === "checkLogin") {
-    fetch(`https://www.last.fm/`, {  credentials: "include" })
-        .then(response => response.text())
-        .then(html => {
-            if(!html.includes('href="/login"')) {
-                sendResponse({loggedIn: true})
-            } else {
-                sendResponse({loggedIn: false})
-            }
-        });
-    return true;
-    }
+  if(msg.action === "addUserToStorage") {
+    chrome.storage.local.get("comparedUsers", (data))
+  }
 });
 
 
