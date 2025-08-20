f29e8bf3f3047798c1a385f3822dd7b300721598
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-20 11:41:08 -0300

    refactor: scrapping

--- background/scrapping.js
+++ background/scrapping.js
@@ -1,10 +1,5 @@
 export async function getCompatibilityScrapping(username) {
-    try {
-        const response = await fetch(`https://www.last.fm/user/${username}`, { credentials: "include" });
-        const html = await response.text();
-        return html;
-
-    } catch (error) {
-        return {error};
-    }
+    const response = await fetch(`https://www.last.fm/user/${username}`, { credentials: "include" });
+    const html = await response.text();
+    return html;
 }
