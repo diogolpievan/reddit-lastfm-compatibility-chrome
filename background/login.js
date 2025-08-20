aa700fc4b8bdc86eee832b199f75e8b54b762afb
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-20 11:40:37 -0300

    refactor: login

--- background/login.js
+++ background/login.js
@@ -1,11 +1,5 @@
 export async function checkLogin() {
-    try {
-        const response = await fetch(`https://www.last.fm/`, { credentials: "include" });
-        const html = await response.text();
-
-        if (!html.includes('href="/login"')) return true;
-        return false;
-    } catch (error) {
-        return error.message;
-    }
+    const response = await fetch(`https://www.last.fm/`, { credentials: "include" });
+    const html = await response.text();
+    return !html.includes('href="/login"');
 }
