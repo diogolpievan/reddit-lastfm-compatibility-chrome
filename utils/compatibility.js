daab6d3b041e4ed98b06b576297f3462ebadd2d2
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-22 10:04:09 -0300

    fix: compatibility ?

--- utils/compatibility.js
+++ utils/compatibility.js
@@ -6,22 +6,23 @@ function getCompatibilityColor(score) {
      "Medium":    "#009966" ,
      "High":      "#ffbb00" ,
      "Very High": "#ff6600" ,
-     "Super":     "#d92323" 
+     "Super":     "#d92323" ,
+     "Not Found": "#000000"
   }
 
   const match = compatibilityColors[score] || "#ffffff03";
   return match;
 }
 
-function parseUserCompatibility(html) {
+function parseAndScrapUserCompatibility(html) {
   const documentHtml = new DOMParser().parseFromString(html, "text/html");
-  const compatibilityScore = documentHtml.querySelector("span.tasteometer-compat-colour").innerText
-  const compatibilityBody  = documentHtml.querySelector(".tasteometer-body").innerHTML.trim();
+  const compatibilityScore = documentHtml.querySelector("span.tasteometer-compat-colour")?.innerText;
+  const compatibilityBody  = documentHtml.querySelector(".tasteometer-body")?.innerHTML.trim();
 
   return {
     compatibility: {
       score: compatibilityScore || "Not Found",
-      body:  compatibilityBody   || null
+      body:  compatibilityBody  || null
     }
   }
 }
