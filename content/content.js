a7032a917923800a761f81ba9fa8dcce80735754
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-26 18:32:39 -0300

    feat: content

--- content/content.js
+++ content/content.js
@@ -1,75 +1,48 @@
-let usersCache = {};
-
-chrome.storage.local.get("comparedUsers", (data) => {
-  if(data.comparedUsers) {
-    usersCache = data.comparedUsers;
-  }
-});
-
-chrome.storage.onChanged.addListener((changes, area) => {
-  if(area === 'local' && changes.comparedUsers) {
-    usersCache = changes.comparedUsers.newValue || {};
-
-  }
-})
 
 function checkUserLogin() {
   chrome.runtime.sendMessage({ action: "checkLogin" }, (resp) => {
-    if (resp.loggedIn) {
-      console.log("Usuário logado!");
+    if (resp.logged) {
+      return true;
     } else {
-      console.log("Usuário não está logado");
+      return false;
     }
   });
 }
 
-function getUserCompatibilityScrapping(username, callback) {
-  chrome.runtime.sendMessage({ action: "getCompatibility", username }, (resp) => {
-      if (resp){
-        callback(resp);
+let isLogged = checkUserLogin();
+const clientLastfmUsername = '';
+
+function getUserCompatibility(usernames, callback) {
+
+  if (usersCache[usernames.lastfmUsername]) {
+    console.log("get of cache")
+    callback(usersCache[usernames.lastfmUsername]);
+  } else {
+    console.log("get of scrapp")
+    const { lastfmUsername } = usernames;
+    chrome.runtime.sendMessage({ action: "getUserCompatibilityScrapping", username: lastfmUsername }, (resp) => {
+
+      if (resp.html) {
+        const scrappedCompatibilityData = parseAndScrapUserCompatibility(resp.html);
+
+        const comparedUser = {
+          ...usernames,
+          ...scrappedCompatibilityData
+        };
+
+        callSaveUserInStorage(comparedUser);
+        callback(scrappedCompatibilityData);
       } else {
         console.error(resp.error);
-        callback(null);
       }
-    }
-  );
-}
-
-function getUserInStorage(username) {
-  return usersCache[username] || null;
-}
-
-const allUserFlair = document.querySelectorAll("author-flair-event-handler");
-
-allUserFlair.forEach((redditUserFlair) => {
-
-  const redditUserFlairText = redditUserFlair.innerText;
-  const lastfmUsername = formatLastfmUserFlair(redditUserFlairText);
-
-  const userAvatar = redditUserFlair
-                      .parentElement
-                      .parentElement
-                      .parentElement
-                      .parentElement
-                      .parentElement
-                      .querySelector("div[slot='commentAvatar']").querySelector('a span');
-  const userMeta = redditUserFlair
-                      .parentElement
-                      .parentElement
-                      .parentElement
-                      .parentElement
-                      .parentElement
-                      .querySelector("div[slot='commentMeta']")
-
-  const userData = getUserInStorage(lastfmUsername);
-  if(!userData) {
-    getUserCompatibilityScrapping(lastfmUsername, (data) => userData = data);
+    });
   }
+}
 
-  userAvatar.classList.add("tasteometer-compat-avatar");
-  userAvatar.style.borderColor = getCompatibilityColor(userData.compatibility.score);
+function callSaveUserInStorage(comparedUser) {
+  chrome.runtime.sendMessage({ action: "saveComparedUser", comparedUser }, (resp) => {
 
-  
+  })
+}
 
-})
-  
+let userFlairQuery = '';
