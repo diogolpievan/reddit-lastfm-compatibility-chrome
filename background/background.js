b061b238cbaa0c46ff6077c2bb580bd505cd9281
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-26 18:36:07 -0300

    fix: spa problem redditr

--- background/background.js
+++ background/background.js
@@ -1,3 +1,20 @@
 import { handleMessages } from "./messages.js";
 
 chrome.runtime.onMessage.addListener(handleMessages);
+
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+    if(details.url.includes("r/lastfm/")) {
+        chrome.scripting.executeScript({
+            target: {tabId: details.tabId},
+            files: [ "content/post.js"]
+        })
+    }  
+
+    if(details.url.includes("comments")) {
+        chrome.scripting.executeScript({
+            target: {tabId: details.tabId},
+            files: ["content/comments.js"]
+        })
+    }  
+})
+ 
