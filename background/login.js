1c08f3edee392bccbd71a412589d1eb27e657722
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-10-02 12:22:07 -0300

    refactor: initial commit

--- background/login.js
+++
@@ -1,5 +0,0 @@
-export async function checkLogin() {
-    const response = await fetch(`https://www.last.fm/`, { credentials: "include" });
-    const html = await response.text();
-    return !html.includes('href="/login"');
-}
--- background/scrapping.js
+++
@@ -1,5 +0,0 @@
-export async function getUserCompatibilityScrapping(username) {
-    const response = await fetch(`https://www.last.fm/user/${username}`, { credentials: "include" });
-    const html = await response.text();
-    return html;
-}
