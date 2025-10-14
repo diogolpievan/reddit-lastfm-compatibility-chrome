adeaaaff33c0b2d0581aa8466d5e69afd10b4978
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-10-14 11:29:47 -0300

    adjustment: scrapping finalized

--- src/background/scrapping.ts
+++ src/background/scrapping.ts
@@ -1,20 +1,33 @@
 import scrapeIt from "scrape-it";
 import ScrappedData from "../types/scrapped-data";
+import { CompatibilityScore } from "../types/compatibility";
 
-export async function getUserCompatibilityScrapping(username: string): Promise<ScrappedData> {
-  const response = await fetch(`https://www.last.fm/user/${username}/`, { credentials: 'include'});
+export async function getUserCompatibilityScrapping(
+  username: string
+): Promise<ScrappedData> {
+  const response = await fetch(`https://www.last.fm/user/${username}/`, {
+    credentials: "include",
+  });
   const html = await response.text();
 
-  const scrappedCompatibility = scrapeIt.scrapeHTML<ScrappedData>(html, {
+  const scrappedCompatibility = scrapeIt.scrapeHTML<{
+    score: string | "Not Found";
+    body: string | null;
+  }>(html, {
     score: {
       selector: "span.tasteometer-compat-colour",
-      how: "text"
+      how: "text",
     },
     body: {
       selector: ".tasteometer-body",
-      how: "html"
-    }
+      how: "html",
+    },
   });
 
-  return scrappedCompatibility;
+  return {
+    compatibility: {
+      score: scrappedCompatibility.score as CompatibilityScore,
+      body: scrappedCompatibility.body,
+    },
+  };
 }
