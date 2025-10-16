33c7fb77ec0b6535dbf00b34566459e3e17c097a
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-10-16 02:34:56 -0300

    WIP: handle-messages wrong

--- src/background/background.ts
+++
@@ -1,3 +0,0 @@
-import browser from "webextension-polyfill";
-
-browser.runtime.onMessage.addListener(handleMessages);
--- src/background/handle-messages.ts
+++ src/background/handle-messages.ts
@@ -1,2 +1,24 @@
+import { getUserCompatibilityScrapping } from "./scrapping";
+import { saveUserInStorage } from "./storage";
 
-export const handleMessages = () => {}
+export function handleMessages(
+  message: any,
+  sender: any,
+  sendResponse: (response?: any) => void
+) {
+  switch (message.action) {
+    case "getUserCompatibilityScrapping":
+      getUserCompatibilityScrapping(message.username)
+        .then((compatibilityData) => sendResponse({ data: compatibilityData }))
+        .catch((err) => sendResponse({ error: err.message }));
+
+      return true;
+
+    case "saveUserInStorage":
+      saveUserInStorage(message.comparedUser)
+        .then((success) => sendResponse(success))
+        .catch((error) => sendResponse({ error: error.message }));
+    default:
+      sendResponse({ error: "Action not exist" });
+  }
+}
