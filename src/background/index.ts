7060d4e2805d8109e04350fab415dbef421362c9
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-10-16 02:32:42 -0300

    change background.ts to index.ts

---
+++ src/background/index.ts
@@ -0,0 +1,4 @@
+import browser from "webextension-polyfill";
+import { getUserCompatibilityScrapping } from "./scrapping";
+
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {});
